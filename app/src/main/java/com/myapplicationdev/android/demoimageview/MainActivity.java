397145f17b9a415f46516f33af9b1bd7f40f1e46
Michell <19030685@myrp.edu.sg>
2021-04-22 10:51:38 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.myapplicationdev.android.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay2, ivDay3, ivDay4, ivDay5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        ivDay3 = (ImageView) findViewById(R.id.imageView3);
+        ivDay4 = (ImageView) findViewById(R.id.imageView4);
+        ivDay5 = (ImageView) findViewById(R.id.imageView5);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay3.setImageResource(R.drawable.day3);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5.setImageResource(R.drawable.day5);
+    }
+}
